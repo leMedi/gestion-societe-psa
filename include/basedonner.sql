5adda7ffb2982d1de57ccc7f9d52456fba20f93b
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-12-09 21:52:17 +0000

    clients sql: jeu de donner

--- include/basedonner.sql
+++ include/basedonner.sql
@@ -2,14 +2,17 @@ DROP DATABASE spa;
 CREATE DATABASE spa;
 USE spa;
 
-CREATE TABLE client(
-   id         int           NOT NULL     AUTO_INCREMENT,
+CREATE TABLE clients(
+   id       INT             NOT NULL    AUTO_INCREMENT,
    nom      VARCHAR (20)    NOT NULL,
    prenom   VARCHAR (20)    NOT NULL,
    tel      INT             NOT NULL,
    address  VARCHAR (40)    NOT NULL,
-   email    VARCHAR (100)   NOT NULL CHECK (email LIKE '%_@__%.__%'),
-   societe  VARCHAR (100)             NOT NULL,
+   email    VARCHAR (100)   NOT NULL    CHECK (email LIKE '%_@__%.__%'),
+   societe  VARCHAR (100)   NOT NULL,
    password VARCHAR (30)    NOT NULL,
    PRIMARY KEY (id)
 );
+
+INSERT INTO clients (nom, prenom, tel, address, email, societe, password) VALUES ('ElHaij', 'Mehdi', 06004868, 'ENSA heda 3amala', 'mehdi@ensa.com', 'ENSA', 'helloworld');
+INSERT INTO clients (nom, prenom, tel, address, email, societe, password) VALUES ('ElYaacoubi', 'Achraf', 06112233, 'ENSA heda 3amala', 'achraf@ensa.com', 'ENSA', 'helloworld');
