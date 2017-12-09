4a2e4f4bd253baf35f4a59471abaf6f08abaf4e6
achraf elyaacoubi <achraf18el@gmail.com>
2017-12-09 22:05:32 +0000

    ajout du emplyers model

--- include/Acteurs.php
+++ include/Acteurs.php
@@ -2,6 +2,30 @@
 
 require_once('outils.php');
 
+/**
+* Model employers
+*/
+class Employers extends basedonner
+{
+    protected $table = 'employers';
+    protected $columns = array('nom', 'prenom', 'tel','email','address','salaire','password','admin');
+
+    function __construct($donner = null)
+    {
+        Parent::__construct($this->table, $this->columns);
+
+        if($donner){
+            $this->remplire($donner);
+        }
+
+    }
+
+    public function estAdmin()
+    {
+        return $this->admin == 0 ? false : true;
+    }
+    
+}
 
 
 
