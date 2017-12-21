71869c7f4b102235405780a6423e568a92341248
achraf elyaacoubi <achraf18el@gmail.com>
2017-12-21 23:32:47 +0000

    comande info designe

--- Commande/info.php
+++ Commande/info.php
@@ -29,84 +29,87 @@ template('header');
 <div class="app-content content container-fluid">
     <div class="content-wrapper">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-6">
                 <div class="card">
                     <div class="card-header">
-                        <h4 class="card-title">commande: <?=$_GET['id']?></h4>
+                        <h4 class="card-title"> <i class="icon-paper"></i> commande: <?=$_GET['id']?> </h4>
                         <a class="heading-elements-toggle"><i class="icon-ellipsis font-medium-3"></i></a>
                     </div>
                     <div class="card-body collapse in">
                         <div class="card-block">
-                            <h4 class="form-section"><i class="icon-paper"></i></h4>
+                            <h4 class="form-section"></h4>
                                 <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Numero de la Commande  :&nbsp;</label>
-                                            <label><?= $commande->numero ?></label>
-                                        </div>
+                                    <div class="col-md-5">
+                                        <h6 class="blue-grey">Numero de la Commande  :</h6>
+                                    </div>
+                                    <div class="col-md-7">
+                                        <h5><?= $commande->numero ?></h5>
                                     </div>
                                 </div>
+                              
                                 <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Projet  :&nbsp;</label>
-                                            <label ><?= $commande->projet?></label>
-                                        </div>
+                                    <div class="col-md-5">
+                                        <h6 class="blue-grey">Projet :</h6>
+                                    </div>
+                                    <div class="col-md-7">
+                                        <h5><?= $commande->projet?></h5>
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Date de Commande  :&nbsp;</label>
-                                            <label ><?= $commande->date_commande?></label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Date de Livraison  :&nbsp;</label>
-                                            <label ><?= $commande->date_livraison?></label>
-                                            </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Client  :&nbsp;</label>
-                                            <label ><?= $commande->client?></label>
-                                        </div>
-                                    </div>
-                                </div>
-                            
-                                                       
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Personne à contacter  :&nbsp;</label>
-                                            <label ><?= $commande->contact?></label>                                            
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Commantaire  :&nbsp;</label>
-                                            <label ><?= $commande->commentaire?></label>                                            
-                                        </div>
-                                    </div>
-                                </div>
-                        </div>
 
-                        <div class="form-actions right">
-                            <a type="submit" class="btn btn-outline-primary" href="<?= lien('/commande/modifier.php?id=' . $commande->id)?>">
-                                <i class="icon-cog"></i> Modifier
-                            </a>
+                                <div class="row">
+                                    <div class="col-md-5">
+                                        <h6 class="blue-grey">Date de Commande :</h6>
+                                    </div>
+                                    <div class="col-md-7">
+                                        <h5><?= $commande->date_commande?></h5>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-5">
+                                        <h6 class="blue-grey" >Date de Livraison :</h6>
+                                    </div>
+                                    <div class="col-md-7">
+                                        <h5><?= $commande->date_livraison?></h5>
+                                    </div>
+                                </div>
+                              
+                                <div class="row">
+                                    <div class="col-md-5">
+                                        <h6 class="blue-grey">Client  :</h6>
+                                    </div>
+                                    <div class="col-md-7">
+                                        <h5><?= $commande->client?></h5>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-5">
+                                        <h6 class="blue-grey">Personne à contacter   :</h6>
+                                    </div>
+                                    <div class="col-md-7">
+                                        <h5><?= $commande->contact?></h5>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-5">
+                                        <h6 class="blue-grey">Commentaire  :</h6>
+                                    </div>
+                                    <div class="col-md-7">
+                                        <h5><?= $commande->commentaire?></h5>
+                                    </div>
+                                </div>
+                                <div class=" text-xs-right " style="margin-right: 25px">
+                            <a href="<?= lien('/commande/modifier.php?id=' . $commande->id)?>" class="btn btn-outline-success">Modifier</a>
                         </div>
+                    </div>
+
+                        
                         
                     </div><!-- .body -->
                 </div><!-- .card -->
-            </div><!-- .col-8 -->
+            </div><!-- .col-6 -->
         </div><!-- .row -->
     </div><!-- .wrapper -->
 </div> <!-- .content -->
