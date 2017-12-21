ab6bbf82e0b46f3156d9eeb2b609cbb0697bfdc0
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-12-21 11:33:46 +0000

    pages with no sidebar & navigation bar

--- templates/header.php
+++ templates/header.php
@@ -38,4 +38,4 @@
     <link rel="stylesheet" type="text/css" href="<?= lien('/public/css/style.css') ?>">
     <!-- END Custom CSS-->
   </head>
-  <body data-open="click" data-menu="vertical-menu" data-col="2-columns" class="vertical-layout vertical-menu 2-columns  fixed-navbar">
+  <body data-open="click" data-menu="vertical-menu" data-col="2-columns" class="<?= (isset($noMenu) || $noMenu == true) ? '1-columns  blank-page blank-page ' : 'vertical-layout vertical-menu 2-columns fixed-navbar '; ?>">
