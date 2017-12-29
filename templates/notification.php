f0ebb435c943ecd9fe44bcdf377b87edabb43aa8
achraf elyaacoubi <achraf18el@gmail.com>
2017-12-29 00:22:00 +0000

    create notification template

---
+++ templates/notification.php
@@ -0,0 +1,16 @@
+<li class="list-group scrollable-container">
+  <a href="javascript:void(0)" class="list-group-item">
+    <div class="media">
+      <div class="media-left valign-middle">
+        <i class="icon-cart3 icon-bg-circle bg-cyan"></i>
+      </div>
+      <div class="media-body">
+        <h6 class="media-heading"><?= $title ?></h6>
+        <p class="notification-text font-small-3 text-muted"><?= $body ?></p>
+        <small>
+          <time datetime="2015-06-11T18:29:20+08:00" class="media-meta text-muted">30 minutes ago</time>
+        </small>
+      </div>
+    </div>
+  </a>
+</li>
